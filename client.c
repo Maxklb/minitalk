20bf4f3ac8e0416b850be1b5735324f81ceb27e6
Maxklb <themaxklb@gmail.com>
2024-04-30 00:05:56 +0200

    test

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 16:46:24 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/04/29 16:40:35 by makoch-l         ###   ########.fr       */
+/*   Updated: 2024/04/29 23:46:46 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,7 @@ void	send_signal(int pid, unsigned char character)
 	unsigned char	temp_char;
 
 	i = 8; // 8 bits dans 1 octet
-	temp_char = character;
-	while (i > 0)
-	{
-		i--;
-		temp_char = character >> i;
-		if (temp_char % 2 == 0)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		usleep(42);
-	}
+
+	kill(pid, SIGUSR2);
+	kill(pid, SIGUSR1);
 }
